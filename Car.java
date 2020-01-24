b0533782786d84504e34a2c1aff015a308f6aaa0
Maks333Kot <59770486+Maks333Kot@users.noreply.github.com>
2020-01-24 12:34:27 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -2,17 +2,17 @@ package com.company;
 
 public abstract class Car {
 
-    String model;
-    String color;
-    int speed;
+    String model = "Model: rx-7";
+    String color = "Color: Red";
+    String speed = "MaxSpeed: 240 km/h";
 
 
     void gas(){
-        System.out.println("gas: " );
+        System.out.println("gas: " + 100);
     }
 
     void brake(){
-        System.out.println("brake: " );
+        System.out.println("brake: " + 20);
     }
 
     abstract void description();
